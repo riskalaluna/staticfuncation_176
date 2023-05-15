ee1e57653bbe8fc86a185e435ef7c5debebddc57
riskalaluna <riskalaluna@gmail.com>
2023-05-15 15:41:02 +0700

    void mahasiswa

---
+++ staticfuncation_176/staticfuncation_176.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+}
