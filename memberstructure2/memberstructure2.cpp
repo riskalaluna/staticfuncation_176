51887aeed7372a24f298874f88df635507a7db76
riskalaluna <riskalaluna@gmail.com>
2023-05-15 15:54:35 +0700

    void mahasiswa

--- memberstructure2/memberstructure2.cpp
+++ memberstructure2/memberstructure2.cpp
@@ -1,20 +1,25 @@
-// memberstructure2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+	static int getNim() { return nim; /*Definisi Function*/ }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
